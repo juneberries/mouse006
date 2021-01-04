dae2a1cd7dc1a9cd9986ea6f43f2804ff6a8a905
June <68424466+juneberries@users.noreply.github.com>
2021-01-04 13:05:01 +0000

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
+def on_button_pressed_a():
+    global on
+    on = 0
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global on
+    kitronik_motor_driver.motor_off(kitronik_motor_driver.Motors.MOTOR1)
+    kitronik_motor_driver.motor_off(kitronik_motor_driver.Motors.MOTOR2)
+    on = 1
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 dis = 0
-dis2 = 0
+on = 0
+kitronik_motor_driver.motor_off(kitronik_motor_driver.Motors.MOTOR1)
+kitronik_motor_driver.motor_off(kitronik_motor_driver.Motors.MOTOR2)
+on = 1
+trnds = 50
 
 def on_forever():
-    global dis2
-    if 50 > dis:
-        dis2 = dis
-        basic.pause(100)
-        if dis2 > dis:
-            basic.show_icon(IconNames.SMALL_HEART)
-            kitronik_motor_driver.motor_off(kitronik_motor_driver.Motors.MOTOR1)
-            kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
-                kitronik_motor_driver.MotorDirection.REVERSE,
-                50)
-    else:
-        basic.show_icon(IconNames.HEART)
-        kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
-            kitronik_motor_driver.MotorDirection.REVERSE,
-            50)
-        kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
-            kitronik_motor_driver.MotorDirection.FORWARD,
-            40)
-basic.forever(on_forever)
-
-def on_forever2():
     global dis
     pins.digital_write_pin(DigitalPin.P2, 0)
     control.wait_micros(2)
@@ -30,4 +25,39 @@ def on_forever2():
     control.wait_micros(10)
     pins.digital_write_pin(DigitalPin.P2, 0)
     dis = pins.pulse_in(DigitalPin.P1, PulseValue.HIGH) / 36
+basic.forever(on_forever)
+
+def on_forever2():
+    if on == 0:
+        if trnds >= dis:
+            basic.pause(20)
+            if trnds / 5 >= dis:
+                kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
+                    kitronik_motor_driver.MotorDirection.FORWARD,
+                    50)
+                kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
+                    kitronik_motor_driver.MotorDirection.REVERSE,
+                    40)
+                basic.pause(500)
+                kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
+                    kitronik_motor_driver.MotorDirection.FORWARD,
+                    100)
+                kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
+                    kitronik_motor_driver.MotorDirection.FORWARD,
+                    100)
+                basic.pause(500)
+            elif trnds > dis:
+                kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
+                    kitronik_motor_driver.MotorDirection.FORWARD,
+                    100)
+                kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
+                    kitronik_motor_driver.MotorDirection.FORWARD,
+                    100)
+        else:
+            kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
+                kitronik_motor_driver.MotorDirection.REVERSE,
+                50)
+            kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
+                kitronik_motor_driver.MotorDirection.FORWARD,
+                40)
 basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     on = 0
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
     kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
     on = 1
@@ -12,7 +14,8 @@ kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
 kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
 on = 1
 let trnds = 50
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     pins.digitalWritePin(DigitalPin.P2, 0)
     control.waitMicros(2)
     pins.digitalWritePin(DigitalPin.P2, 1)
@@ -20,22 +23,27 @@ basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P2, 0)
     dis = pins.pulseIn(DigitalPin.P1, PulseValue.High) / 36
 })
-basic.forever(function () {
-    if (trnds >= dis) {
-        basic.pause(20)
-        if (trnds / 5 >= dis) {
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 50)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 40)
-            basic.pause(500)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 100)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
-            basic.pause(500)
-        } else if (trnds > dis) {
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 100)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
+basic.forever(function on_forever2() {
+    if (on == 0) {
+        if (trnds >= dis) {
+            basic.pause(20)
+            if (trnds / 5 >= dis) {
+                kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 50)
+                kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 40)
+                basic.pause(500)
+                kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 100)
+                kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
+                basic.pause(500)
+            } else if (trnds > dis) {
+                kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 100)
+                kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
+            }
+            
+        } else {
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 50)
+            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 40)
         }
-    } else {
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 50)
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 40)
+        
     }
+    
 })
